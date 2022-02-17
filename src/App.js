b63a1463e12424a5d2a35855781e37ff0bd70105
fahimehahmadi <fahimehahmadi94@gmail.com>
2022-02-17 15:07:17 +0330

    add ant Design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import { Select } from "antd";
+import { Button, Tooltip } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+
+const { Option } = Select;
+
+function handleChange(value) {
+  console.log(`selected ${value}`);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Select
+        defaultValue="lucy"
+        style={{ width: 120 }}
+        onChange={handleChange}
+      >
+        <Option value="jack">Jack</Option>
+        <Option value="lucy">Lucy</Option>
+        <Option value="disabled" disabled>
+          Disabled
+        </Option>
+        <Option value="Yiminghe">yiminghe</Option>
+      </Select>
+    <br></br>
+    <Tooltip title="search">
+      <Button type="primary" shape="circle" icon={<SearchOutlined />} size="large" />
+    </Tooltip>
+    </>
   );
 }
 
